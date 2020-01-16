5bfc19cd44f415f657d39e89d3cacf679233f710
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-16 09:06:42 +0100

    up

--- src/app/header1/header1.component.ts
+++ src/app/header1/header1.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SharedDataService } from './../../services/shared-data.service';
 
 @Component({
-  selector: 'app-header1',
-  templateUrl: './header1.component.html',
-  styleUrls: ['./header1.component.css']
+  selector: 'solution-header1',
+  templateUrl: 'header1.component.html'
 })
-export class Header1Component implements OnInit {
+export class Header1Component {
+  // -----------------------------------------------------------------------//
+  private title = 'Angular 4';
+  @Input() angularVersion: string = '';
+  @Input() materialVersion: string = '';
+  private websiteUrl: string = 'https://iamfaisal.live/';  
+  // -----------------------------------------------------------------------//
+  constructor(private readonly _sharedDataService: SharedDataService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onThemeChange(event){
+    this._sharedDataService.OnThemeSwitch.next(event.checked);
   }
-
 }
